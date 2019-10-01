edc7f53491947070039c6dc0f22d3233ffaf080d
nikhilmishra03 <56030038+nikhilmishra03@users.noreply.github.com>
2019-10-01 19:20:20 +0530

    Update Swap.cpp
    Added comments

--- Swap.cpp
+++ Swap.cpp
@@ -1,10 +1,10 @@
 #include <iostream.h>
 using namespace std;
 
-int main()
+int main()//main function
 {
 	int a, b;
-	cout<<"Enter two numbers : ";
+	cout<<"Enter two numbers : ";//accepting values
 	cin>>a>>b;
 	a = a + b;
 	b = a - b;
