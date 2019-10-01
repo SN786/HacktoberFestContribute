baa4ffbbc5e8b63d61bd8f450f6435a65e5b3582
nikhilmishra03 <56030038+nikhilmishra03@users.noreply.github.com>
2019-10-01 19:18:42 +0530

    Update swap.java
    addded more comments

--- swap.java
+++ swap.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
+import java.util.Scanner;//scanner class
 
 public class swap {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int a=s.nextInt();
-        int b=s.nextInt();
+        int b=s.nextInt();//Accepting values
 
         //Swapping without a temporary variable
         a=b-a;
         b=b-a;
         a=b+a;
         
-        System.out.println(a + " " + b);
+        System.out.println(a + " " + b);//Printing
     }
 }
