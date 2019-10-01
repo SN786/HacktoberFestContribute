1b424f07bf738bacd375edc5c385b4a23e09744d
HarshMarolia <53485461+HarshMarolia@users.noreply.github.com>
2019-10-01 19:10:22 +0530

    Update calculator.c
    Added comments

--- calculator.c
+++ calculator.c
@@ -2,16 +2,16 @@
 
 int main(void)
 {
-  char operator;
+  char operator;//Initializing variables
     double n1,n2;
 
     printf("Enter an operator (+, -, *,/): ");
-    scanf("%c", &operator);
+    scanf("%c", &operator);//Accepting values
 
     printf("Enter two numbers: ");
     scanf("%lf  %lf",&n1, &n2);
 
-  switch(operator)
+  switch(operator)//Switch case
   {
    case '+':
         printf("%lf + %lf = %lf ", n1, n2, n1 + n2);
@@ -29,7 +29,7 @@ int main(void)
         printf("%lf / %lf = %lf ", n1, n2, n1 / n2);
         break;
   }
-  printf("\n");
+  printf("\n");//Printing statements
   return 0;
    }
 
